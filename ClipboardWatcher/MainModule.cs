7f5a5a1a742202b91dcfc0df9aea99c1bf76a17b
cristibadila <Cristi.Badila@gmail.com>
2013-03-02 11:21:17 +0200

    added a separate project to contain the clipboard wrapper and added ninject to mange dependency injection

---
+++ ClipboardWatcher/MainModule.cs
@@ -0,0 +1,12 @@
+﻿using Ninject.Modules;
+
+namespace ClipboardWatcher
+{
+    public class MainModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Kernel.Bind<MainForm>().To<MainForm>();
+        }
+    }
+}
---
+++ ClipboardWrapper/ClipboardWrapperModule.cs
@@ -0,0 +1,12 @@
+﻿using Ninject.Modules;
+
+namespace ClipboardWrapper
+{
+    public class ClipboardWrapperModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Kernel.Bind<IClipboardManager>().To<ClipboardManager>();
+        }
+    }
+}
