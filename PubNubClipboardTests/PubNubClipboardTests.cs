063334209c634eb6a6cf015cbcee74fdb1fb101f
cristibadila <Cristi.Badila@gmail.com>
2013-05-20 12:20:51 +0300

    fixed some failinf tests

--- PubNubClipboardTests/PubNubClipboardTests.cs
+++ PubNubClipboardTests/PubNubClipboardTests.cs
@@ -15,6 +15,8 @@ namespace PubNubClipboardTests
         private Mock<IConfigurationService> _mockConfigurationService;
         private Mock<IPubNubClientFactory> _mockPubNubClientFactory;
 
+        private PubNubClipboard _subject;
+
         [SetUp]
         public void Setup()
         {
@@ -22,43 +24,42 @@ namespace PubNubClipboardTests
             var communicationSettings = new CommunicationSettings();
             _mockConfigurationService.Setup(x => x.CommunicationSettings).Returns(communicationSettings);
             _mockPubNubClientFactory = new Mock<IPubNubClientFactory>();
+            this._subject = new PubNubClipboard(_mockConfigurationService.Object, _mockPubNubClientFactory.Object);
         }
 
         [Test]
-        public void Ctor_CommunicationsChannelFromConfigurationServiceIsEmpty_SetsIsInitializedFalse()
+        public void Initialize_CommunicationsChannelFromConfigurationServiceIsEmpty_SetsIsInitializedFalse()
         {
             var communicationSettings = new CommunicationSettings { Channel = string.Empty };
             _mockConfigurationService.Setup(x => x.CommunicationSettings).Returns(communicationSettings);
 
-            var omniclipboard = new PubNubClipboard(_mockConfigurationService.Object, _mockPubNubClientFactory.Object);
+            _subject.Initialize();
 
-            omniclipboard.IsInitialized.Should().BeFalse();
+            _subject.IsInitialized.Should().BeFalse();
         }
 
         [Test]
-        public void Ctor_CommunicationsChannelFromConfigurationIsNotEmpty_CallsClientFactoryCreate()
+        public void Initialize_CommunicationsChannelFromConfigurationIsNotEmpty_CallsClientFactoryCreate()
         {
             var settings = new CommunicationSettings { Channel = "asd" };
-            var communicationSettings = settings;
-            _mockConfigurationService.Setup(x => x.CommunicationSettings).Returns(communicationSettings);
+            _mockConfigurationService.Setup(x => x.CommunicationSettings).Returns(settings);
             _mockPubNubClientFactory.Setup(x => x.Create()).Returns(new Pubnub("test", "test"));
 
-            var pubNubClipboard = new PubNubClipboard(_mockConfigurationService.Object, _mockPubNubClientFactory.Object);
+            _subject.Initialize();
 
-            pubNubClipboard.Should().NotBeNull();
             _mockPubNubClientFactory.Verify(x => x.Create(), Times.Once());
         }
 
         [Test]
-        public void Ctor_CommunicationsChannelFromConfigurationServiceIsNotEmptyAndFactoryCreatesAClient_SetsIsInitializedTrue()
+        public void Initialize_CommunicationsChannelFromConfigurationServiceIsNotEmptyAndFactoryCreatesAClient_SetsIsInitializedTrue()
         {
             var communicationSettings = new CommunicationSettings { Channel = "asda" };
             _mockConfigurationService.Setup(x => x.CommunicationSettings).Returns(communicationSettings);
             _mockPubNubClientFactory.Setup(x => x.Create()).Returns(new Pubnub("test", "test"));
 
-            var omniclipboard = new PubNubClipboard(_mockConfigurationService.Object, _mockPubNubClientFactory.Object);
+            _subject.Initialize();
 
-            omniclipboard.IsInitialized.Should().BeTrue();
+            _subject.IsInitialized.Should().BeTrue();
         }
     }
 }
