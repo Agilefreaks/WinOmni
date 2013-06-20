0bd7a3b1c281b7e39f04743add130c544e374df9
cristibadila <Cristi.Badila@gmail.com>
2013-06-20 18:42:31 +0300

    adds the implementation of the LoadLocalConfiguration step

--- OmniCommon/Services/ActivationServiceData/ActivationServiceSteps/LoadLocalConfiguration.cs
+++ OmniCommon/Services/ActivationServiceData/ActivationServiceSteps/LoadLocalConfiguration.cs
@@ -1,12 +1,27 @@
 ﻿namespace OmniCommon.Services.ActivationServiceData.ActivationServiceSteps
 {
-    using System;
+    using OmniCommon.Interfaces;
 
     public class LoadLocalConfiguration : ActivationStepBase
     {
+        private readonly IConfigurationService _configurationService;
+
+        public LoadLocalConfiguration(IConfigurationService configurationService)
+        {
+            _configurationService = configurationService;
+        }
+
         public override IExecuteResult Execute()
         {
-            throw new NotImplementedException();
+            _configurationService.LoadCommunicationSettings();
+
+            return new ExecuteResult
+                       {
+                           State =
+                               string.IsNullOrEmpty(_configurationService.CommunicationSettings.Channel)
+                                   ? SimpleStepStateEnum.Failed
+                                   : SimpleStepStateEnum.Successful
+                       };
         }
     }
 }
