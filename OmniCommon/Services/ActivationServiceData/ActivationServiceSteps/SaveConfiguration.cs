c90c5f8432799f86a11c1b230ff8c41d62f334f8
cristibadila <Cristi.Badila@gmail.com>
2013-06-21 17:32:52 +0300

    adds the SaveConfiguration step implementation

--- OmniCommon/Services/ActivationServiceData/ActivationServiceSteps/SaveConfiguration.cs
+++ OmniCommon/Services/ActivationServiceData/ActivationServiceSteps/SaveConfiguration.cs
@@ -1,10 +1,24 @@
 ﻿namespace OmniCommon.Services.ActivationServiceData.ActivationServiceSteps
 {
+    using OmniCommon.Interfaces;
+
     public class SaveConfiguration : ActivationStepBase
     {
+        private readonly IConfigurationService _configurationService;
+
+        private readonly string _channel;
+
+        public SaveConfiguration(IConfigurationService configurationService, string payload)
+        {
+            _configurationService = configurationService;
+            _channel = payload;
+        }
+
         public override IExecuteResult Execute()
         {
-            throw new System.NotImplementedException();
+            _configurationService.UpdateCommunicationChannel(_channel);
+
+            return new ExecuteResult { State = SingleStateEnum.Successful };
         }
     }
 }
---
+++ OmniCommonTests/Services/ActivationServiceData/ActivationServiceSteps/SaveConfigurationTests.cs
@@ -0,0 +1,37 @@
+﻿namespace OmniCommonTests.Services.ActivationServiceData.ActivationServiceSteps
+{
+    using FluentAssertions;
+    using Moq;
+    using NUnit.Framework;
+    using OmniCommon.Interfaces;
+    using OmniCommon.Services.ActivationServiceData.ActivationServiceSteps;
+
+    [TestFixture]
+    public class SaveConfigurationTests
+    {
+        private SaveConfiguration _subject;
+
+        private Mock<IConfigurationService> _mockConfigurationService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockConfigurationService = new Mock<IConfigurationService>();
+            _subject = new SaveConfiguration(_mockConfigurationService.Object, "testChannel");
+        }
+
+        [Test]
+        public void Execute_Always_ReturnsAResultWithStatusSuccessful()
+        {
+            _subject.Execute().State.Should().Be(SingleStateEnum.Successful);
+        }
+
+        [Test]
+        public void Execute_Always_CallsConfigurationServiceUpdateCommnuicationChannelWithThePayloadObject()
+        {
+            _subject.Execute();
+
+            _mockConfigurationService.Verify(x => x.UpdateCommunicationChannel("testChannel"));
+        }
+    }
+}
