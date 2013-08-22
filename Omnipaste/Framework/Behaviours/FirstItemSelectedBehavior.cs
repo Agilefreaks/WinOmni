f0cc5a797841df8f0e679bd0713e693bc5a3c830
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2013-08-22 15:41:23 +0300

    automatically select first item from history

---
+++ Omnipaste/Framework/Behaviours/FirstItemSelectedBehavior.cs
@@ -0,0 +1,28 @@
+﻿namespace Omnipaste.Framework.Behaviours
+{
+    using System;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Windows.Controls;
+    using System.Windows.Controls.Primitives;
+    using System.Windows.Interactivity;
+
+    public class FirstItemSelectedBehavior : Behavior<Selector>
+    {
+        protected override void OnAttached()
+        {
+            var itemSourcePropertyDescriptor = DependencyPropertyDescriptor.FromProperty(ItemsControl.ItemsSourceProperty, typeof(Selector));
+
+            itemSourcePropertyDescriptor.AddValueChanged(AssociatedObject, SelectFirstItem);
+        }
+
+        private void SelectFirstItem(object sender, EventArgs e)
+        {
+            if (AssociatedObject.Items != null)
+            {
+                var item = AssociatedObject.Items.Cast<object>().FirstOrDefault();
+                AssociatedObject.SelectedItem = item;
+            }
+        }
+    }
+}
