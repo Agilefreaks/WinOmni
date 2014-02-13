2e44f09e5ebc26ec78c30236d75bc1d932726ac7
adi <levelboy@gmail.com>
2014-02-13 10:38:45 +0200

    Removed OldOmniService

--- OmniClipboard/OmniClipboardModule.cs
+++ OmniClipboard/OmniClipboardModule.cs
@@ -2,13 +2,11 @@
 {
     using Ninject.Modules;
     using global::OmniClipboard.Messaging;
-    using OmniCommon.Interfaces;
 
     public class OmniClipboardModule : NinjectModule
     {
         public override void Load()
         {
-            Kernel.Bind<IOmniClipboard>().To<OmniClipboard>().InSingletonScope();
             Kernel.Bind<IPubNubClientFactory>().To<PubNubClientFactory>().InSingletonScope();
             Kernel.Bind<IMessagingService>().To<PubNubMessagingService>();
         }
