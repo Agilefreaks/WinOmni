a51793d5a2b0b69af21b2aeba127f7b75bfbbdf3
Calin <calinoiu.alexandru@agilefreaks.com>
2014-05-29 15:29:16 +0300

    moves projects around and updates references

---
+++ Code/Omnipaste/Framework/FlyoutBaseViewModel.cs
@@ -0,0 +1,97 @@
+using Caliburn.Micro;
+using MahApps.Metro.Controls;
+
+namespace Omnipaste.Framework
+{
+    public abstract class FlyoutBaseViewModel : Screen, IFlyoutViewModel
+    {
+        #region private fields
+        private string _header;
+        private bool _isOpen;
+        private Position _position;
+        private bool _isModal;
+        private bool _isPinned;
+        #endregion
+
+        public string Header
+        {
+            get
+            {
+                return _header;
+            }
+            set
+            {
+                if (_header != value)
+                {
+                    _header = value;
+                    NotifyOfPropertyChange(() => Header);
+                }
+            }
+        }
+
+        public bool IsOpen
+        {
+            get
+            {
+                return _isOpen;
+            }
+            set
+            {
+                if (_isOpen != value)
+                {
+                    _isOpen = value;
+                    NotifyOfPropertyChange(() => IsOpen);
+                }
+            }
+        }
+
+        public bool IsModal
+        {
+            get
+            {
+                return _isModal;
+            }
+            set
+            {
+                if (_isModal != value)
+                {
+                    _isModal = value;
+                    NotifyOfPropertyChange(() => IsModal);
+                }
+            }
+        }
+
+        public bool IsPinned
+        {
+            get
+            {
+                return _isPinned;
+            }
+            set
+            {
+                if (_isPinned != value)
+                {
+                    _isPinned = value;
+                    NotifyOfPropertyChange(() => IsPinned);
+                }
+            }
+        }
+
+        public Position Position
+        {
+            get
+            {
+                return _position;
+            }
+            set
+            {
+                if (_position != value)
+                {
+                    _position = value;
+                    NotifyOfPropertyChange(() => Position);
+                }
+                
+            }
+        }
+    }
+}
