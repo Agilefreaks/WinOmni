e97bb2ae176fce9a79c00c70458d20cca18c7ccc
Calin <calinoiu.alexandru@agilefreaks.com>
2014-06-04 09:16:22 +0300

    [wo-37] removes CommunicationSettings

--- Code/OmniCommon/Services/CommunicationSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OmniCommon.Services
-{
-    public class CommunicationSettings
-    {
-        public string Channel { get; set; }
-
-        public string DeviceIdentifier { get; set; }
-    }
-}
