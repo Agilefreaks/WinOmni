ef564faf4e483b0ffceae9fcfc1d6538a774bbdd
adi <levelboy@gmail.com>
2014-06-06 17:34:55 +0300

    Fixed failing test

--- Code/Tests/OmnipasteTests/ViewModels/LoadingViewModelTests.cs
+++ Code/Tests/OmnipasteTests/ViewModels/LoadingViewModelTests.cs
@@ -3,11 +3,13 @@
     using System.Threading;
     using Caliburn.Micro;
     using FluentAssertions;
+    using Moq;
     using NUnit.Framework;
     using OmniCommon.EventAggregatorMessages;
     using Omnipaste.Dialog;
     using Omnipaste.EventAggregatorMessages;
     using Omnipaste.Loading;
+    using Omnipaste.UserToken;
 
     [TestFixture]
     public class LoadingViewModelTests
@@ -16,11 +18,14 @@
 
         private IEventAggregator _eventAggregator;
 
+        private Mock<IUserTokenViewModel> _mockUserTokenViewModel;
+
         [SetUp]
         public void SetUp()
         {
             _eventAggregator = new EventAggregator();
-            _subject = new LoadingViewModel(_eventAggregator);
+            _mockUserTokenViewModel = new Mock<IUserTokenViewModel>();
+            _subject = new LoadingViewModel(_eventAggregator) { UserTokenViewModel = _mockUserTokenViewModel.Object };
             var parent = new DialogViewModel();
             parent.ActivateItem(_subject);
         }
@@ -37,6 +42,14 @@
             _eventAggregator.PublishOnCurrentThread(new GetTokenFromUserMessage());
 
             _subject.State.Should().Be(LoadingViewModelStateEnum.AwaitingUserTokenInput);
+        }
+
+        [Test]
+        public void Handle_GetTokenFromUser_SetsTheMessageOnTheUserTokenViewModel()
+        {
+            _eventAggregator.PublishOnCurrentThread(new GetTokenFromUserMessage { Message = "message" });
+
+            _mockUserTokenViewModel.VerifySet(vm => vm.Message = "message");
         }
 
         [Test]
