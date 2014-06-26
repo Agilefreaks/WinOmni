e0e895cb908800d36a2df2c1cd11812e258ec20e
adi <levelboy@gmail.com>
2014-06-26 08:05:20 +0300

    Added clippingViewModel

---
+++ Code/Omnipaste/Clipping/ClippingViewModel.cs
@@ -0,0 +1,59 @@
+﻿namespace Omnipaste.Clipping
+{
+    using Caliburn.Micro;
+    using Clipboard.Enums;
+    using Clipboard.Models;
+
+    public class ClippingViewModel : Screen, IClippingViewModel
+    {
+        #region Fields
+
+        private Clipping _model;
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        public ClippingViewModel(Clipping model)
+        {
+            Model = model;
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        public bool IsCloudVisible
+        {
+            get
+            {
+                return Model.Source == ClippingSourceEnum.Web;
+            }
+        }
+
+        public bool IsLaptopVisible
+        {
+            get
+            {
+                return Model.Source == ClippingSourceEnum.Local;
+            }
+        }
+
+        public Clipping Model
+        {
+            get
+            {
+                return _model;
+            }
+            set
+            {
+                _model = value;
+                NotifyOfPropertyChange(() => Model);
+                NotifyOfPropertyChange(() => IsCloudVisible);
+                NotifyOfPropertyChange(() => IsLaptopVisible);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Code/Omnipaste/Clipping/IClippingViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Omnipaste.Clipping
+{
+    using Caliburn.Micro;
+    using Clipboard.Models;
+
+    public interface IClippingViewModel : IScreen
+    {
+        Clipping Model { get; set; }
+    }
+}
