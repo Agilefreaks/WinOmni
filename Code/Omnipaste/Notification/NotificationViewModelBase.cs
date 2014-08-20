7eaac3053385c3fe253d882174503062b3475113
adi <levelboy@gmail.com>
2014-08-20 09:34:57 +0300

    Separated logic for closing the notifications

--- Code/Omnipaste/Notification/NotificationViewModelBase.cs
+++ Code/Omnipaste/Notification/NotificationViewModelBase.cs
@@ -4,12 +4,51 @@
 
     public abstract class NotificationViewModelBase: Screen, INotificationViewModel
     {
+        private ViewModelStatusEnum _state;
+
         #region Public Properties
 
         public string Message { get; set; }
 
         public abstract string Title { get; }
 
+        public ViewModelStatusEnum State
+        {
+            get
+            {
+                return _state;
+            }
+            set
+            {
+                if (value == _state)
+                {
+                    return;
+                }
+                _state = value;
+                NotifyOfPropertyChange(() => State);
+            }
+        }
+
+        protected override void OnActivate()
+        {
+            base.OnActivate();
+
+            State = ViewModelStatusEnum.Open;
+        }
+        
+        public virtual void Close()
+        {
+            State = ViewModelStatusEnum.Closed;
+            //((IConductor)Parent).DeactivateItem(this, true);
+        }
+
         #endregion
+    }
+
+    public enum ViewModelStatusEnum
+    {
+        Open,
+
+        Closed
     }
 }
