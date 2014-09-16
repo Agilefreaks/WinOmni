6c74ae221b4e880b32184e5d64ffc682d124b42f
cristi-badila <Cristi.Badila@gmail.com>
2014-09-16 19:00:05 +0300

    [#32] adds OmnipasteInstaller project which can install Omnipaste on a users's machine in "AppData\Local\Programs"

---
+++ Code/Installer/InstallerCustomActions/CustomAction.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Deployment.WindowsInstaller;
+
+namespace InstallerCustomActions
+{
+    public class CustomActions
+    {
+        [CustomAction]
+        public static ActionResult CustomAction1(Session session)
+        {
+            session.Log("Begin CustomAction1");
+
+            return ActionResult.Success;
+        }
+    }
+}
