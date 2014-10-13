b7eb209f1527900a3aafa5cc39f15fbb52453505
cristi-badila <Cristi.Badila@gmail.com>
2014-10-13 16:03:00 +0300

    [#183] minor refactoring in the CustomActions folder

--- Code/Installer/InstallerCustomActions/CustomAction.cs
+++ Code/Installer/InstallerCustomActions/CustomAction.cs
@@ -9,15 +9,16 @@ namespace InstallerCustomActions
 
     public class CustomActions
     {
-        private static readonly Regex AuthorizationKeyRegex = new Regex(@"^(.*)(\d{6})$");
-
         [CustomAction]
         public static ActionResult StartAppWithAuthorizationKey(Session session)
         {
             var result = ActionResult.Success;
             try
             {
-                StartApp(ExtractAuthorizationKey(session), session);
+                var targetDir = session.CustomActionData["TargetDir"];
+                var target = session.CustomActionData["Target"];
+                var msiFileName = session.CustomActionData["OriginalDatabase"];
+                AuthorizationBootstrapper.StartApp(Path.Combine(targetDir, target), msiFileName);
             }
             catch (Exception exception)
             {
@@ -26,33 +27,6 @@ namespace InstallerCustomActions
             }
 
             return result;
-        }
-
-        private static void StartApp(string authorizationKey, Session session)
-        {
-            var targetDir = session.CustomActionData["TargetDir"];
-            var target = session.CustomActionData["Target"];
-            var targetPath = Path.Combine(targetDir, target);
-
-            var arguments = string.Format("-authorizationKey={0}", authorizationKey);
-            Process.Start(new ProcessStartInfo(targetPath) { Arguments = arguments, WorkingDirectory = targetDir, });
-        }
-
-        private static string ExtractAuthorizationKey(Session session)
-        {
-            var authorizationKey = string.Empty;
-            var msiFileName = session.CustomActionData["OriginalDatabase"];
-
-            if (msiFileName == null) return authorizationKey;
-
-            var fileName = Path.GetFileNameWithoutExtension(msiFileName);
-            var matchCollection = AuthorizationKeyRegex.Matches(fileName);
-            if (matchCollection.Count > 0)
-            {
-                authorizationKey = matchCollection[0].Groups[2].Captures[0].Value;
-            }
-
-            return authorizationKey;
         }
     }
 }
