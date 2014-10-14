a6574d2899454231f0ee4431fbe08924920e258c
cristi-badila <Cristi.Badila@gmail.com>
2014-10-14 10:20:51 +0300

    [#183] adds the click once Uninstaller code used in ClickOnceMigration to the InstallerCustomActions project

---
+++ Code/Installer/InstallerCustomActions/ClickOnceUninstaller/IUninstallStep.cs
@@ -0,0 +1,14 @@
+﻿namespace InstallerCustomActions.ClickOnceUninstaller
+{
+    using System;
+    using System.Collections.Generic;
+
+    public interface IUninstallStep : IDisposable
+    {
+        void Prepare(List<string> componentsToRemove);
+
+        void PrintDebugInformation();
+
+        void Execute();
+    }
+}
