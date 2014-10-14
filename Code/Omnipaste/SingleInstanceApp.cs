41ddd977f7bb7b5fc4b884d2867c3cd76946e1a3
cristi-badila <Cristi.Badila@gmail.com>
2014-10-14 12:25:51 +0300

    [#183] removes old implementation of SingleInstance app in favor of a simpler version as the old implementation would sometimes throw exceptions during the installation process

---
+++ Code/Omnipaste/SingleInstanceApp.cs
@@ -0,0 +1,34 @@
+﻿namespace Omnipaste
+{
+    using System;
+    using System.Threading;
+
+    public class SingleInstanceApp : IDisposable
+    {
+        private readonly Mutex _mutex;
+
+        public bool IsFirstInstance { get; private set; }
+
+        private SingleInstanceApp(string uniqueId)
+        {
+            bool createdNew;
+            _mutex = new Mutex(true, uniqueId, out createdNew);
+            IsFirstInstance = createdNew;
+        }
+
+        public static bool InitializeAsFirstInstance(string uniqueId, out SingleInstanceApp instance)
+        {
+            instance = new SingleInstanceApp(uniqueId);
+
+            return instance.IsFirstInstance;
+        }
+
+        public void Dispose()
+        {
+            if (_mutex != null)
+            {
+                _mutex.Close();
+            }
+        }
+    }
+}
