5d76ee7cf193eeddeeb13d34e2bd198588eaeae7
cristi-badila <Cristi.Badila@gmail.com>
2014-10-24 14:55:17 +0300

    [#188] modifies the DebugHeaderView so as to be visible only if the setting in the configuration files permits it

--- Code/Omnipaste/Shell/DebugHeader/DebugHeaderViewModel.cs
+++ Code/Omnipaste/Shell/DebugHeader/DebugHeaderViewModel.cs
@@ -1,16 +1,50 @@
 ﻿namespace Omnipaste.Shell.DebugHeader
 {
     using Ninject;
+    using OmniCommon;
+    using OmniCommon.Interfaces;
     using Omnipaste.Shell.Debug;
 
     public class DebugHeaderViewModel : IDebugHeaderViewModel
     {
+        #region Fields
+
+        private readonly IConfigurationService _configurationService;
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        public DebugHeaderViewModel(IConfigurationService configurationService)
+        {
+            _configurationService = configurationService;
+        }
+
+        #endregion
+
+        #region Public Properties
+
         [Inject]
         public IDebugBarViewModel DebugBarViewModel { get; set; }
+
+        public bool IsAvailable
+        {
+            get
+            {
+                bool value;
+                return bool.TryParse(_configurationService[ConfigurationProperties.ShowDebugBar], out value) && value;
+            }
+        }
+
+        #endregion
+
+        #region Public Methods and Operators
 
         public void ToggleDebugBarFlyout()
         {
             DebugBarViewModel.IsOpen = !DebugBarViewModel.IsOpen;
         }
+
+        #endregion
     }
 }
