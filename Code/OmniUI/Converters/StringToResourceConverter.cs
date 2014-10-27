19af8ed05fa2337e4f170c11af2273ab33d4ff68
cristi-badila <Cristi.Badila@gmail.com>
2014-10-27 16:25:49 +0200

    [#188] renames the StringToVisualBrushConverter into StringToResource converter

--- Code/OmniUI/Converters/StringToResourceConverter.cs
+++ Code/OmniUI/Converters/StringToResourceConverter.cs
@@ -4,20 +4,19 @@
     using System.Globalization;
     using System.Windows;
     using System.Windows.Data;
-    using System.Windows.Media;
 
-    public class StringToVisualBrushConverter : IValueConverter
+    public class StringToResourceConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            VisualBrush result = null;
+            object result = null;
             var iconName = value as string;
             if (!string.IsNullOrWhiteSpace(iconName))
             {
-                result = Application.Current.FindResource(iconName) as VisualBrush;
+                result = Application.Current.FindResource(iconName);
             }
 
-            return result ?? new VisualBrush();
+            return result;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
