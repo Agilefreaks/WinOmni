65943f99a0ee4d7ca0acc9b9b9acb44e6aa7988b
cristi-badila <Cristi.Badila@gmail.com>
2014-11-03 12:36:30 +0200

    [#198] adds a ShowConnectionTroubleshooter activation step

---
+++ Code/Omnipaste/Services/ActivationServiceData/ActivationServiceSteps/ShowConnectionTroubleshooter.cs
@@ -0,0 +1,36 @@
+﻿namespace Omnipaste.Services.ActivationServiceData.ActivationServiceSteps
+{
+    using System;
+    using System.Reactive.Linq;
+    using Caliburn.Micro;
+
+    public class ShowConnectionTroubleshooter : ActivationStepBase
+    {
+        #region Fields
+
+        private readonly IEventAggregator _eventAggregator;
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        public ShowConnectionTroubleshooter(IEventAggregator eventAggregator)
+        {
+            _eventAggregator = eventAggregator;
+            _eventAggregator.Subscribe(this);
+        }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        public override IObservable<IExecuteResult> Execute()
+        {
+            _eventAggregator.PublishOnCurrentThread(new ShowConnectionTroubleshooterMessage());
+
+            return Observable.Never<IExecuteResult>();
+        }
+
+        #endregion
+    }
+}
