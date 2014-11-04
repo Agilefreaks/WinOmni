d54bc498785e1c90751b56ce96447ca13f8881dd
cristi-badila <Cristi.Badila@gmail.com>
2014-11-04 11:07:03 +0200

    [#198] fixes issue with http proxy password not being saved

--- Code/Omnipaste/Loading/ConnectionTroubleshooter/ConnectionTroubleshooterViewModel.cs
+++ Code/Omnipaste/Loading/ConnectionTroubleshooter/ConnectionTroubleshooterViewModel.cs
@@ -3,6 +3,8 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Windows;
+    using System.Windows.Controls;
     using Caliburn.Micro;
     using OmniCommon;
     using OmniCommon.EventAggregatorMessages;
