ddd196792ff816bfd292c02ccd6ec8ba3c5ae831
cristi-badila <Cristi.Badila@gmail.com>
2014-11-21 18:30:30 +0200

    [#187] transforms the custom label template into a custom control

---
+++ Code/Omnipaste/Controls/ExpandableTextBlock.cs
@@ -0,0 +1,33 @@
+﻿namespace Omnipaste.Controls
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    public class ExpandableTextBlock : Control
+    {
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+            "Text",
+            typeof(string),
+            typeof(ExpandableTextBlock),
+            new PropertyMetadata(default(string)));
+
+        public string Text
+        {
+            get
+            {
+                return (string)GetValue(TextProperty);
+            }
+            set
+            {
+                SetValue(TextProperty, value);
+            }
+        }
+
+        static ExpandableTextBlock()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(ExpandableTextBlock),
+                new FrameworkPropertyMetadata(typeof(ExpandableTextBlock)));
+        }
+    }
+}
