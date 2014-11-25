0869dea9f888385530f44d5e6ba7549c5d5a7dcd
cristi-badila <Cristi.Badila@gmail.com>
2014-11-25 20:07:56 +0200

    [#247] adds qr code image containing android install link on the android install guide

--- Code/Omnipaste/Loading/AndroidInstallGuide/AndroidInstallGuideViewModel.cs
+++ Code/Omnipaste/Loading/AndroidInstallGuide/AndroidInstallGuideViewModel.cs
@@ -3,11 +3,22 @@
     using System;
     using System.ComponentModel;
     using System.Diagnostics;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
     using Caliburn.Micro;
+    using Gma.QrCodeNet.Encoding;
+    using Gma.QrCodeNet.Encoding.Windows.Render;
+    using OmniCommon.Helpers;
 
     public class AndroidInstallGuideViewModel : Screen, IAndroidInstallGuideViewModel
     {
+        #region Fields
+
         private Uri _androidInstallLink;
+
+        private ImageSource _qrCodeImageData;
+
+        #endregion
 
         #region Constructors and Destructors
 
@@ -34,10 +45,28 @@
                 }
                 _androidInstallLink = value;
                 NotifyOfPropertyChange();
+                UpdateQRCodeImageData();
             }
         }
 
         public IEventAggregator EventAggregator { get; set; }
+
+        public ImageSource QRCodeImageData
+        {
+            get
+            {
+                return _qrCodeImageData;
+            }
+            set
+            {
+                if (Equals(value, _qrCodeImageData))
+                {
+                    return;
+                }
+                _qrCodeImageData = value;
+                NotifyOfPropertyChange();
+            }
+        }
 
         #endregion
 
@@ -53,6 +82,32 @@
             {
                 // Looks like there is no way for us to act on this
             }
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void UpdateQRCodeImageData()
+        {
+            System.Action update = () => QRCodeImageData = RenderQRCodeImage();
+            DispatcherProvider.Application.Dispatch(update);
+        }
+
+        private ImageSource RenderQRCodeImage()
+        {
+            var encoder = new QrEncoder(ErrorCorrectionLevel.M);
+            QrCode qrCode;
+            encoder.TryEncode(Convert.ToString(AndroidInstallLink), out qrCode);
+            var wRenderer = new WriteableBitmapRenderer(
+                new FixedModuleSize(4, QuietZoneModules.Two),
+                Colors.Black,
+                Colors.White);
+
+            var imageSource = new WriteableBitmap(116, 116, 96, 96, PixelFormats.Gray8, null);
+            wRenderer.Draw(imageSource, qrCode.Matrix);
+
+            return imageSource;
         }
 
         #endregion
