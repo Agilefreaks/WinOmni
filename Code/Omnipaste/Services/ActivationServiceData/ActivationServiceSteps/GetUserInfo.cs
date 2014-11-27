f273993b40c94d4c6a383902218d17ff01f83455
cristi-badila <Cristi.Badila@gmail.com>
2014-11-27 11:55:20 +0200

    [#246] updates the GetUserInfo activation step to fail on the first encountered exception

--- Code/Omnipaste/Services/ActivationServiceData/ActivationServiceSteps/GetUserInfo.cs
+++ Code/Omnipaste/Services/ActivationServiceData/ActivationServiceSteps/GetUserInfo.cs
@@ -3,28 +3,11 @@
     using System;
     using System.Reactive.Linq;
     using OmniApi.Resources.v1;
-    using OmniCommon.ExtensionMethods;
     using OmniCommon.Helpers;
 
     public class GetUserInfo : ActivationStepBase
     {
-        #region Constants
-
-        private const int DefaultRetryCount = 3;
-
-        #endregion
-
-        #region Static Fields
-
-        private static readonly TimeSpan DefaultRetryDelay = TimeSpan.FromSeconds(1);
-
-        #endregion
-
         #region Fields
-
-        private readonly int _retryCount;
-
-        private readonly TimeSpan _retryDelay;
 
         private readonly IUserInfo _userInfo;
 
@@ -33,15 +16,8 @@
         #region Constructors and Destructors
 
         public GetUserInfo(IUserInfo userInfo)
-            : this(userInfo, DefaultRetryDelay, DefaultRetryCount)
-        {
-        }
-
-        public GetUserInfo(IUserInfo userInfo, TimeSpan retryDelay, int retryCount)
         {
             _userInfo = userInfo;
-            _retryDelay = retryDelay;
-            _retryCount = retryCount;
         }
 
         #endregion
@@ -51,8 +27,7 @@
         public override IObservable<IExecuteResult> Execute()
         {
             return
-                Observable.Defer(() => _userInfo.Get().ReportErrors())
-                    .RetryAfter(_retryDelay, _retryCount)
+                _userInfo.Get()
                     .Select(userInfo => new ExecuteResult { State = SimpleStepStateEnum.Successful, Data = userInfo })
                     .Catch<IExecuteResult, Exception>(
                         exception =>
--- Code/Tests/OmnipasteTests/Services/ActivationServiceData/ActivationServiceSteps/GetUserInfoTests.cs
+++ Code/Tests/OmnipasteTests/Services/ActivationServiceData/ActivationServiceSteps/GetUserInfoTests.cs
@@ -22,17 +22,11 @@
 
         private UserInfo _userInfo;
 
-        private TimeSpan _retryDelay;
-
-        private int _retryCount;
-
         [SetUp]
         public void Setup()
         {
             _mockUserInfo = new Mock<IUserInfo>();
-            _retryDelay = TimeSpan.FromTicks(100);
-            _retryCount = 3;
-            _subject = new GetUserInfo(_mockUserInfo.Object, _retryDelay, _retryCount);
+            _subject = new GetUserInfo(_mockUserInfo.Object);
             _testScheduler = new TestScheduler();
             SchedulerProvider.Default = _testScheduler;
             _userInfo = new UserInfo();
@@ -63,66 +57,21 @@
         }
 
         [Test]
-        public void Execute_WhenSubscribedToAndGetUserInfoFails_ReportErrors()
+        public void Execute_WhenSubscribedToAndGetUserInfoFails_WillCompletesWithFailedResult()
         {
             var exception = new Exception("test");
             var getUserInfoObservable =
                 _testScheduler.CreateColdObservable(
                     new Recorded<Notification<UserInfo>>(100, Notification.CreateOnError<UserInfo>(exception)),
                     new Recorded<Notification<UserInfo>>(200, Notification.CreateOnCompleted<UserInfo>()));
-            _mockUserInfo.Setup(x => x.Get()).Returns(getUserInfoObservable);
-            var mockExceptionReporter = new Mock<IExceptionReporter>();
-            ExceptionReporter.Instance = mockExceptionReporter.Object;
-
-            _testScheduler.Start(_subject.Execute, TimeSpan.FromSeconds(1).Ticks);
-
-            mockExceptionReporter.Verify(x => x.Report(exception));
-        }
-
-
-        [Test]
-        public void Execute_WhenSubscribedToAndGetUserInfoFails_WillTryAgainAndIfAResultIsObtainedCompletesSuccessfully()
-        {
-            var getUserInfoObservable1 =
-                _testScheduler.CreateColdObservable(
-                    new Recorded<Notification<UserInfo>>(100, Notification.CreateOnError<UserInfo>(new Exception("test"))),
-                    new Recorded<Notification<UserInfo>>(200, Notification.CreateOnCompleted<UserInfo>()));
-            var getUserInfoObservable2 =
-                _testScheduler.CreateColdObservable(
-                    new Recorded<Notification<UserInfo>>(100, Notification.CreateOnError<UserInfo>(new Exception("test"))),
-                    new Recorded<Notification<UserInfo>>(200, Notification.CreateOnCompleted<UserInfo>()));
-            var getUserInfoObservable3 =
-                _testScheduler.CreateColdObservable(
-                    new Recorded<Notification<UserInfo>>(100, Notification.CreateOnNext(_userInfo)),
-                    new Recorded<Notification<UserInfo>>(200, Notification.CreateOnCompleted<UserInfo>()));
-            var observables = new[] { getUserInfoObservable1, getUserInfoObservable2, getUserInfoObservable3 };
-            var callCount = 0;
-            _mockUserInfo.Setup(x => x.Get()).Returns(() => observables[callCount++]);
-
-            var testableObserver = _testScheduler.Start(_subject.Execute, 3 * (200 + _retryDelay.Ticks));
-
-            _mockUserInfo.Verify(x => x.Get(), Times.Exactly(3));
-            testableObserver.Messages.Count.Should().Be(2);
-            testableObserver.Messages[0].Value.Kind.Should().Be(NotificationKind.OnNext);
-            testableObserver.Messages[0].Value.Value.State.Should().Be(SimpleStepStateEnum.Successful);
-            testableObserver.Messages[0].Value.Value.Data.Should().Be(_userInfo);
-            testableObserver.Messages[1].Value.Kind.Should().Be(NotificationKind.OnCompleted);
-        }
-
-        [Test]
-        public void Execute_WhenSubscribedToAndGetUserInfoFailsMoreThanTheAllowedNumberOfRetries_WillCompleteWithAFailedResult()
-        {
-            var getUserInfoObservable =
-                _testScheduler.CreateColdObservable(
-                    new Recorded<Notification<UserInfo>>(100, Notification.CreateOnError<UserInfo>(new Exception("test"))),
-                    new Recorded<Notification<UserInfo>>(200, Notification.CreateOnCompleted<UserInfo>()));
             _mockUserInfo.Setup(x => x.Get()).Returns(() => getUserInfoObservable);
 
-            var testableObserver = _testScheduler.Start(_subject.Execute, (_retryCount + 1) * (200 + _retryDelay.Ticks));
+            var testableObserver = _testScheduler.Start(_subject.Execute, TimeSpan.FromSeconds(1).Ticks);
 
             testableObserver.Messages.Count.Should().Be(2);
             testableObserver.Messages[0].Value.Kind.Should().Be(NotificationKind.OnNext);
             testableObserver.Messages[0].Value.Value.State.Should().Be(SimpleStepStateEnum.Failed);
+            testableObserver.Messages[0].Value.Value.Data.Should().Be(exception);
             testableObserver.Messages[1].Value.Kind.Should().Be(NotificationKind.OnCompleted);
         }
     }
