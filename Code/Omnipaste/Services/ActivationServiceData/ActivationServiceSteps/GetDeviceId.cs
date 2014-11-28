708162fa3f31142c623e8e28255909aea74b2e64
cristi-badila <Cristi.Badila@gmail.com>
2014-11-28 15:44:51 +0200

    [#261] adds a GetDeviceId activation step

---
+++ Code/Omnipaste/Services/ActivationServiceData/ActivationServiceSteps/GetDeviceId.cs
@@ -0,0 +1,43 @@
+﻿namespace Omnipaste.Services.ActivationServiceData.ActivationServiceSteps
+{
+    using OmniCommon.Interfaces;
+
+    public class GetDeviceId : SynchronousStepBase
+    {
+        #region Fields
+
+        private readonly IConfigurationService _configurationService;
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        public GetDeviceId(IConfigurationService configurationService)
+        {
+            _configurationService = configurationService;
+        }
+
+        #endregion
+
+        #region Methods
+
+        protected override IExecuteResult ExecuteSynchronously()
+        {
+            var result = new ExecuteResult();
+            var deviceIdentifier = _configurationService.DeviceIdentifier;
+            if (string.IsNullOrWhiteSpace(deviceIdentifier))
+            {
+                result.State = SimpleStepStateEnum.Failed;
+            }
+            else
+            {
+                result.State = SimpleStepStateEnum.Successful;
+                result.Data = deviceIdentifier;
+            }
+
+            return result;
+        }
+
+        #endregion
+    }
+}
