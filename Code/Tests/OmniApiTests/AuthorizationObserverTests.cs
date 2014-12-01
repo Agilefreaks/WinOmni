ad97549943ef6cb6b6d5cbd7fdfb2fa311ec47e0
cristi-badila <Cristi.Badila@gmail.com>
2014-12-01 18:22:34 +0200

    [#267] cleans up the code in AuthorizationObserver making clearer to understand

--- Code/Tests/OmniApiTests/AuthorizationObserverTests.cs
+++ Code/Tests/OmniApiTests/AuthorizationObserverTests.cs
@@ -72,7 +72,7 @@
         }
 
         [Test]
-        public void Authorize_WhenErrorIsHttpRequestException_WillCallOauth2Refresh()
+        public void Authorize_WhenErrorIsHttpRequestException_WillCallOAuth2Refresh()
         {
             _mockOAuth2.Setup(m => m.Refresh(It.IsAny<string>())).Returns(Observable.Empty<Token>());
 
@@ -107,6 +107,30 @@
             refreshObservable.OnError(createBadRequestException.Result);
 
             _mockSessionManager.Verify(m => m.LogOut(), Times.Once);
+        }
+
+        [Test]
+        public void Authorize_WhenRefreshThrowsRequestException_ForwardsTheGivenError()
+        {
+            var refreshObservable = new Subject<Token>();
+            _mockOAuth2.Setup(m => m.Refresh(It.IsAny<string>())).Returns(refreshObservable);
+
+            AuthorizationObserver
+                .Authorize(_observable, _mockOAuth2.Object, _mockSessionManager.Object, new Token("access token", "refresh token"))
+                .Subscribe(_testableObserver);
+
+            var createUnauthorizedException = ApiException.Create(new HttpResponseMessage(HttpStatusCode.Unauthorized));
+            createUnauthorizedException.Wait();
+            var createBadRequestException = ApiException.Create(new HttpResponseMessage(HttpStatusCode.BadRequest));
+            createBadRequestException.Wait();
+
+            _observable.OnError(createUnauthorizedException.Result);
+            refreshObservable.OnError(createBadRequestException.Result);
+
+            _testableObserver.Messages.Should()
+                .Contain(
+                    m =>
+                    m.Value.Kind == NotificationKind.OnError && m.Value.Exception == createBadRequestException.Result);
         }
     }
 }
