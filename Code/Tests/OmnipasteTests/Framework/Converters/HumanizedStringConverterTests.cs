2b1572602bbd705f29fb05104f71bcd46272ba79
cristi-badila <Cristi.Badila@gmail.com>
2014-12-04 16:08:05 +0200

    [#269] adds the activity time stamp to the activity view as a humanized string

---
+++ Code/Tests/OmnipasteTests/Framework/Converters/HumanizedStringConverterTests.cs
@@ -0,0 +1,68 @@
+﻿namespace OmnipasteTests.Framework.Converters
+{
+    using System;
+    using System.Globalization;
+    using FluentAssertions;
+    using NUnit.Framework;
+    using Omnipaste.Framework.Converters;
+
+    [TestFixture]
+    public class HumanizedStringConverterTests
+    {
+        private HumanizedStringConverter _subject;
+
+        [SetUp]
+        public void Setup()
+        {
+            _subject = new HumanizedStringConverter();
+        }
+
+        [Test]
+        public void Convert_ValueIsCamelCaseString_ReturnsHumanizedString()
+        {
+            _subject.Convert("SomeCamelCaseString", typeof(string), null, CultureInfo.InvariantCulture)
+                .Should()
+                .Be("Some camel case string");
+        }
+
+        [Test]
+        public void Convert_ValueIsUnderscoredString_ReturnsHumanizedString()
+        {
+            _subject.Convert("Some_underscored_string", typeof(string), null, CultureInfo.InvariantCulture)
+                .Should()
+                .Be("Some underscored string");
+        }
+
+        [Test]
+        public void Convert_ValueIsEnumWithDescription_ReturnsDescriptionOfValue()
+        {
+            _subject.Convert(SomeEnum.ValueWithDescription, typeof(string), null, CultureInfo.InvariantCulture)
+                .Should()
+                .Be("Some description");
+        }
+
+        [Test]
+        public void Convert_ValueIsEnumWithoutDescription_ReturnsValueAsHumanizedString()
+        {
+            _subject.Convert(SomeEnum.ValueWithoutDescription, typeof(string), null, CultureInfo.InvariantCulture)
+                .Should()
+                .Be("Value without description");
+        }
+
+        [Test]
+        public void Convert_ValueIsCurrentDateTime_ReturnsApproximateTimeAgo()
+        {
+            var dateTime = DateTime.UtcNow - TimeSpan.FromDays(5);
+            _subject.Convert(dateTime, typeof(string), null, CultureInfo.InvariantCulture)
+                .Should()
+                .Be("5 days ago");
+        }
+
+        private enum SomeEnum
+        {
+            [System.ComponentModel.Description("Some description")]
+            ValueWithDescription,
+            ValueWithoutDescription
+        }
+    }
+}
