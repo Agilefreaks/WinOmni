f0ca9b12615c72f589d6a2c99c6e6d606a42bea5
cristi-badila <Cristi.Badila@gmail.com>
2014-12-11 21:49:40 +0200

    [#289] extracts a ContactInfoPresenter

--- Code/Omnipaste/Activity/ContactRelatedActivityViewModel.cs
+++ Code/Omnipaste/Activity/ContactRelatedActivityViewModel.cs
@@ -1,32 +1,16 @@
 ﻿namespace Omnipaste.Activity
 {
-    using System;
-    using System.Reflection;
-    using System.Windows.Media;
-    using System.Windows.Media.Imaging;
-    using OmniCommon.Helpers;
-    using Omnipaste.Activity.Models;
+    using Omnipaste.Activity.Presenters;
     using Omnipaste.Services;
+
     using OmniUI.Attributes;
 
     [UseView("Omnipaste.Activity.ActivityView", IsFullyQualifiedName = true)]
     public class ContactRelatedActivityViewModel : ActivityViewModel, IContactRelatedActivityViewModel
     {
-        #region Constants
-
-        private const string DefaultContactImagePathFormat = @"/{0};component/Resources/DefaultUserIcon.png";
-
-        #endregion
-
         #region Fields
 
-        private readonly Uri _defaultContactImageUri;
-
-        private string _contactIdentifier;
-
-        private ImageSource _contactImage;
-
-        private ContactInfo _contactInfo;
+        private IContactInfoPresenter _contactInfo;
 
         #endregion
 
@@ -35,46 +19,25 @@
         public ContactRelatedActivityViewModel(IUiRefreshService uiRefreshService)
             : base(uiRefreshService)
         {
-            var assemblyName = Assembly.GetExecutingAssembly().GetName().Name;
-            _defaultContactImageUri = new Uri(
-                string.Format(DefaultContactImagePathFormat, assemblyName),
-                UriKind.Relative);
         }
 
         #endregion
 
         #region Public Properties
 
-        public string ContactIdentifier
+        public IContactInfoPresenter ContactInfo
         {
             get
             {
-                return _contactIdentifier;
+                return _contactInfo;
             }
             set
             {
-                if (value == _contactIdentifier)
+                if (Equals(value, _contactInfo))
                 {
                     return;
                 }
-                _contactIdentifier = value;
-                NotifyOfPropertyChange();
-            }
-        }
-
-        public ImageSource ContactImage
-        {
-            get
-            {
-                return _contactImage;
-            }
-            set
-            {
-                if (Equals(value, _contactImage))
-                {
-                    return;
-                }
-                _contactImage = value;
+                _contactInfo = value;
                 NotifyOfPropertyChange();
             }
         }
@@ -88,51 +51,8 @@
             set
             {
                 base.Model = value;
-                UpdateContactDetails();
+                ContactInfo = new ContactInfoPresenter(value.ExtraData.ContactInfo);
             }
-        }
-
-        #endregion
-
-        #region Methods
-
-        private static ImageSource GetImageSourceFromUri(Uri uri)
-        {
-            var image = new BitmapImage();
-            image.BeginInit();
-            image.UriSource = uri;
-            image.EndInit();
-
-            return image;
-        }
-
-        private ImageSource GetDefaultUserImage()
-        {
-            return GetImageSourceFromUri(_defaultContactImageUri);
-        }
-
-        private void UpdateContactDetails()
-        {
-            _contactInfo = Model.ExtraData.ContactInfo;
-            UpdateContactImage();
-            UpdateContactIdentifier();
-        }
-
-        private void UpdateContactIdentifier()
-        {
-            ContactIdentifier = string.IsNullOrWhiteSpace(_contactInfo.Name) ? _contactInfo.Phone : _contactInfo.Name;
-        }
-
-        private void UpdateContactImage()
-        {
-            DispatcherProvider.Application.Dispatch(
-                (Action)
-                (() =>
-                    {
-                        ContactImage = _contactInfo.ImageUri != null
-                                           ? GetImageSourceFromUri(_contactInfo.ImageUri)
-                                           : GetDefaultUserImage();
-                    }));
         }
 
         #endregion
