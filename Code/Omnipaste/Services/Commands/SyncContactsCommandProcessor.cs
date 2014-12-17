901ee0070a57434d3008248ada7925f1fb033c1b
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2014-12-17 14:56:11 +0200

    [#306] adds SyncContacts command

---
+++ Code/Omnipaste/Services/Commands/SyncContactsCommandProcessor.cs
@@ -0,0 +1,58 @@
+﻿namespace Omnipaste.Services.Commands
+{
+    using System;
+    using System.Reactive.Linq;
+    using Contacts.Api.Resources.v1;
+    using Contacts.Handlers;
+    using Contacts.Models;
+
+    public class SyncContactsCommand
+    {
+    }
+
+    public class SyncContactsCommandResult
+    {
+        #region Public Properties
+
+        public ContactList ContactList { get; set; }
+
+        #endregion
+    }
+
+    public class SyncContactsCommandProcessor : ICommandProcessor<SyncContactsCommand, SyncContactsCommandResult>
+    {
+        #region Fields
+
+        private readonly IContacts _contacts;
+
+        private readonly IContactsHandler _contactsHandler;
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        public SyncContactsCommandProcessor(IContacts contacts, IContactsHandler contactsHandler)
+        {
+            _contacts = contacts;
+            _contactsHandler = contactsHandler;
+        }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        public IObservable<SyncContactsCommandResult> Process(SyncContactsCommand command)
+        {
+            return
+                _contacts.Get()
+                    .Catch<ContactList, Exception>(
+                        exception =>
+                        _contacts.Sync()
+                            .Select(_ => _contactsHandler)
+                            .Switch())
+                    .Select(contactList => new SyncContactsCommandResult { ContactList = contactList });
+        }
+
+        #endregion
+    }
+}
---
+++ Code/Tests/OmnipasteTests/Services/Commands/SyncContactsCommandProcessorTests.cs
@@ -0,0 +1,87 @@
+﻿namespace OmnipasteTests.Services.Commands
+{
+    using System;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Reactive;
+    using System.Reactive.Linq;
+    using Contacts.Api.Resources.v1;
+    using Contacts.Handlers;
+    using Contacts.Models;
+    using FluentAssertions;
+    using Microsoft.Reactive.Testing;
+    using Moq;
+    using NUnit.Framework;
+    using OmniApi.Models;
+    using Omnipaste.Services.Commands;
+    using Refit;
+
+    [TestFixture]
+    public class SyncContactsCommandProcessorTests
+    {
+        private SyncContactsCommandProcessor _subject;
+
+        private Mock<IContacts> _mockContacts;
+
+        private Mock<IContactsHandler> _mockContactsHandler;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockContacts = new Mock<IContacts> { DefaultValue = DefaultValue.Mock };
+            _mockContactsHandler = new Mock<IContactsHandler> { DefaultValue = DefaultValue.Mock };
+            _subject = new SyncContactsCommandProcessor(_mockContacts.Object, _mockContactsHandler.Object);
+        }
+
+        [Test]
+        public void Process_WhenContactsReturnsContacts_ReturnsCommandResult()
+        {
+            var testScheduler = new TestScheduler();
+            var contactList = new ContactList();
+            _mockContacts.Setup(m => m.Get()).Returns(Observable.Return(contactList));
+
+            var observer = testScheduler.Start(() => _subject.Process(new SyncContactsCommand()));
+
+            observer.Messages.First().Value.Kind.Should().Be(NotificationKind.OnNext);
+            observer.Messages.First().Value.Value.ContactList.Should().Be(contactList);
+        }
+
+        [Test]
+        public void Process_WhenContactsGetFails_CallsSync()
+        {
+            var testScheduler = new TestScheduler();
+            var getObservable =
+                testScheduler.CreateColdObservable(
+                    new Recorded<Notification<ContactList>>(
+                        100,
+                        Notification.CreateOnError<ContactList>(new Exception())));
+            _mockContacts.Setup(m => m.Get()).Returns(getObservable);
+
+            testScheduler.Start(() => _subject.Process(new SyncContactsCommand()));
+            
+            _mockContacts.Verify(m => m.Sync());
+        }
+
+        [Test]
+        public void Process_WhenContactGetFailsSyncSucceedsAndHandlerReturnsValue_ReturnsObservable()
+        {
+            var testScheduler = new TestScheduler();
+            var contactList = new ContactList();
+            var getObservable =
+                testScheduler.CreateColdObservable(
+                    new Recorded<Notification<ContactList>>(
+                        100,
+                        Notification.CreateOnError<ContactList>(new Exception())));
+            _mockContacts.Setup(m => m.Get()).Returns(getObservable);
+            _mockContacts.Setup(m => m.Sync()).Returns(Observable.Return(new EmptyModel()));
+            _mockContactsHandler.Setup(m => m.Subscribe(It.IsAny<IObserver<ContactList>>()))
+                .Callback<IObserver<ContactList>>(o => o.OnNext(contactList));
+
+            var observer = testScheduler.Start(() => _subject.Process(new SyncContactsCommand()));
+
+            observer.Messages.First().Value.Kind.Should().Be(NotificationKind.OnNext);
+            observer.Messages.First().Value.Value.ContactList.Should().Be(contactList);
+        }
+    }
+}
