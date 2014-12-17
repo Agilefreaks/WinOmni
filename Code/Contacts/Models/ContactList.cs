77f20d9363d905419b33bcb4cace091164ab37dc
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2014-12-17 18:50:53 +0200

    [#306] adds EncryptionConverter

--- Code/Contacts/Models/ContactList.cs
+++ Code/Contacts/Models/ContactList.cs
@@ -1,9 +1,12 @@
 ﻿namespace Contacts.Models
 {
     using System.Collections.Generic;
+    using Newtonsoft.Json;
+    using OmniApi.Support.Converters;
 
     public class ContactList
     {
+        [JsonConverter(typeof(EncryptionConverter))]
         public List<Contact> Contacts { get; set; }
     }
 }
