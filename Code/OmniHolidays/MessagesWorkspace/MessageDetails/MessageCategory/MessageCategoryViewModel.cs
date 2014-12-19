c9ac4a8ab760644b560b73e25d6bb759061ff68e
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2014-12-19 18:49:01 +0200

    [#308] populates available languages from message provider's results

--- Code/OmniHolidays/MessagesWorkspace/MessageDetails/MessageCategory/MessageCategoryViewModel.cs
+++ Code/OmniHolidays/MessagesWorkspace/MessageDetails/MessageCategory/MessageCategoryViewModel.cs
@@ -1,6 +1,57 @@
 ﻿namespace OmniHolidays.MessagesWorkspace.MessageDetails.MessageCategory
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Resources;
+    using OmniHolidays.Providers;
+    using global::OmniHolidays.Properties;
+
     public class MessageCategoryViewModel : MessageStepViewModelBase
     {
+        private readonly IMessageDefinitionProvider _messageDefinitionProvider;
+
+        private IList<object> _languages;
+
+        public MessageCategoryViewModel(IMessageDefinitionProvider messageDefinitionProvider)
+        {
+            _messageDefinitionProvider = messageDefinitionProvider;
+        }
+
+        public IList<object> Languages
+        {
+            get
+            {
+                return _languages;
+            }
+            set
+            {
+                if (Equals(value, _languages))
+                {
+                    return;
+                }
+                _languages = value;
+                NotifyOfPropertyChange(() => Languages);
+            }
+        }
+
+        protected override void OnActivate()
+        {
+            base.OnActivate();
+
+            var resourceManager = new ResourceManager(typeof(Resources));
+            _messageDefinitionProvider.Get()
+                .Subscribe(
+                    messages =>
+                        {
+                            Languages =
+                                messages.Select(m => m.Language)
+                                    .Distinct()
+                                    .Select(lang => new { Value = lang, Content = resourceManager.GetString(lang) })
+                                    .Cast<object>()
+                                    .ToList();
+                        },
+                    () => { });
+        }
     }
 }
