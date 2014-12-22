7a7619cb6deaff685ee39372cd7907152a902976
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2014-12-22 12:09:43 +0200

    [#337] updates holiday menu entry style

--- Code/OmniHolidays/MessagesWorkspace/MessagesMainMenuEntryViewModel.cs
+++ Code/OmniHolidays/MessagesWorkspace/MessagesMainMenuEntryViewModel.cs
@@ -1,9 +1,7 @@
 ﻿namespace OmniHolidays.MessagesWorkspace
 {
-    using OmniUI.Attributes;
     using OmniUI.Workspace;
 
-    [UseView(typeof(OmniUI.MainMenuEntry.MainMenuEntryView))]
     public class MessagesMainMenuEntryViewModel :  WorkspaceMainMenuEntry<IMessagesWorkspace>, IMessagesMainMenuEntryViewModel
     {
         public override string Icon
