5cb239bc2e6b52d20b8570d0c6d07b60ce35be26
cristi-badila <Cristi.Badila@gmail.com>
2015-01-08 13:41:59 +0200

    [#355] fixes issues with user images not being displayed correctly in some places in the app

---
+++ Code/Omnipaste/Framework/Converters/ControlToYRadiusConverter.cs
@@ -0,0 +1,31 @@
+﻿namespace Omnipaste.Framework.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+
+    public class ControlToYRadiusConverter : IValueConverter
+    {
+        #region Public Methods and Operators
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var control = value as Control;
+            double yRadius = 0;
+            if (control != null)
+            {
+                yRadius = control.Height / 2;
+            }
+
+            return yRadius;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
