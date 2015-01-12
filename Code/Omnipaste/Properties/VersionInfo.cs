6c03468fde9a948dc5258612f463e68bcfe4744e
cristi-badila <Cristi.Badila@gmail.com>
2015-01-12 15:49:01 +0200

    modifies the way the version number is specified for the main assembly as well as the installer so as to not use a build parameter but instead use the hard coded version for the main assembly

--- Code/Omnipaste/Properties/VersionInfo.cs
+++ Code/Omnipaste/Properties/VersionInfo.cs
@@ -1,3 +1,2 @@
-﻿// Do not change this. The version is set on package builds only by setting the revision MSBuild property
-[assembly: System.Reflection.AssemblyVersion("2.1.0.10")]
-[assembly: System.Reflection.AssemblyFileVersion("2.1.0.10")]
+﻿[assembly: System.Reflection.AssemblyVersion("1.2.0.0")]
+[assembly: System.Reflection.AssemblyFileVersion("1.2.0.0")]
