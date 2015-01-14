381ea0f2baf7bb3acaf1968a9228303b790da285
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2015-01-14 16:02:00 +0200

    [#367] adds activity item added, deleted/restored animation
    This is a combination of 7 commits.
    [#367] fixes re-rendering of view on each item add/remove
    
    [#367] adds item added animation
    
    [#367] adds fade in/out on item shown/hidden
    
    [#367] extracts constant
    
    [#367] adds notification list fluid animation on dismiss
    
    [#367] adds animated content control style to conversation
    
    [#367] adds direction for item added animation in conversation and activity list

---
+++ Code/OmniUI/Converters/NegativeThicknessConverter.cs
@@ -0,0 +1,30 @@
+﻿namespace OmniUI.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+
+    public class NegativeThicknessConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var result = new Thickness(0, 0, 0, 0);
+            if (values != null && values.Length == 2)
+            {
+                var height = (double)values[0];
+                var direction = (VerticalAlignment)values[1];
+                result = direction == VerticalAlignment.Top
+                             ? new Thickness(0, -height, 0, 0)
+                             : new Thickness(0, 0, 0, -height);
+            }
+
+            return result;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
