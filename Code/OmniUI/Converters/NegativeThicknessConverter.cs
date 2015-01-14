0ea1a7325d0fd3b4a2e61c1a66a91520474ceeeb
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2015-01-14 16:20:37 +0200

    [#367] adds item added animation

---
+++ Code/OmniUI/Converters/NegativeThicknessConverter.cs
@@ -0,0 +1,25 @@
+﻿namespace OmniUI.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+
+    public class NegativeThicknessConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double height = 0;
+            if (value != null)
+            {
+                height = (double)value;
+            }
+            return new Thickness(0, -height, 0, 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
