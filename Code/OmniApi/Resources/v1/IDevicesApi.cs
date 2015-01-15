73703fcbb20c6f1064abcc408cdbc8e2b7ce8862
cristi-badila <Cristi.Badila@gmail.com>
2015-01-15 17:51:53 +0200

    [#369] rearranged methods in the IDevicesApi interface alphabetically

--- Code/OmniApi/Resources/v1/IDevicesApi.cs
+++ Code/OmniApi/Resources/v1/IDevicesApi.cs
@@ -9,11 +9,11 @@
     {
         #region Public Methods and Operators
 
-        [Put("/devices")]
-        IObservable<EmptyModel> Update([Body] object deviceParams, [Header("Authorization")] string token);
-
         [Put("/devices/activate")]
         IObservable<Device> Activate([Body] Device device, [Header("Authorization")] string token, [Header("Client-Version")] string version);
+
+        [Post("/devices/call")]
+        IObservable<EmptyModel> Call([AliasAs("phone_number")] string phoneNumber, [Header("Authorization")] string token);
 
         [Post("/devices")]
         IObservable<Device> Create([Body] Device device, [Header("Authorization")] string token);
@@ -21,20 +21,20 @@
         [Put("/devices/deactivate")]
         IObservable<Device> Deactivate([Body] Device device, [Header("Authorization")] string token);
 
+        [Post("/devices/end_call")]
+        IObservable<EmptyModel> EndCall([Header("Authorization")] string token);
+
         [Get("/user/devices")]
         IObservable<List<Device>> GetAll([Header("Authorization")] string token);
 
-        [Post("/devices/end_call")]
-        IObservable<EmptyModel> EndCall([Header("Authorization")] string token);
+        [Delete("/user/devices")]
+        IObservable<EmptyModel> Remove(string identifier, [Header("Authorization")] string accessToken);
 
         [Post("/devices/sms")]
         IObservable<EmptyModel> SendSms([AliasAs("phone_number")] string phoneNumber, string content, [Header("Authorization")] string token);
 
-        [Post("/devices/call")]
-        IObservable<EmptyModel> Call([AliasAs("phone_number")] string phoneNumber, [Header("Authorization")] string token);
-
-        [Delete("/user/devices")]
-        IObservable<EmptyModel> Remove(string identifier, [Header("Authorization")] string accessToken);
+        [Put("/devices")]
+        IObservable<EmptyModel> Update([Body] object deviceParams, [Header("Authorization")] string token);
 
         #endregion
     }
