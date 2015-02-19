09c7750ae0536208f0c5fea323b8e4a96378c3c2
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2015-02-19 10:21:49 +0200

    [#415] adds global exception logger to catch and log unhandled exception

---
+++ Code/Omnipaste/Services/GlobalExceptionLogger.cs
@@ -0,0 +1,26 @@
+﻿namespace Omnipaste.Services
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Threading;
+    using OmniCommon;
+
+    public class GlobalExceptionLogger
+    {
+        public static void Hook()
+        {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+            Application.Current.DispatcherUnhandledException += CurrentOnDispatcherUnhandledException;
+        }
+
+        private static void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs args)
+        {
+            SimpleLogger.Log("Unhandled DOMAIN Exception: " + args.ExceptionObject);
+        }
+
+        private static void CurrentOnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs args)
+        {
+            SimpleLogger.Log("Unhandled DISPATCHER Exception: " + args.Exception);
+        }
+    }
+}
