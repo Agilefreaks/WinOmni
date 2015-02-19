d39363a0c0996e903082e2226954336025a98f95
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2015-02-19 09:10:22 +0200

    [#416] adds filtering options

--- Code/Tests/OmnipasteTests/ClippingList/ClippingListViewModelTests.cs
+++ Code/Tests/OmnipasteTests/ClippingList/ClippingListViewModelTests.cs
@@ -3,6 +3,7 @@
     using System.Linq;
     using System.Reactive;
     using Caliburn.Micro;
+    using Clipboard.Models;
     using FluentAssertions;
     using Microsoft.Reactive.Testing;
     using Moq;
@@ -105,6 +106,101 @@
 
             _subject.Items.Count.Should().Be(1);
             _subject.Items.First().Model.BackingModel.Should().Be(modifiedClipping);
+        }
+
+        [Test]
+        public void FilteredItems_NoFilterIsApplied_ReturnsAllItems()
+        {
+            var clippingPresenter1 = new ClippingPresenter(new ClippingModel { Content = "test1" });
+            var clippingPresenter2 = new ClippingPresenter(new ClippingModel { Content = "Other" });
+            ((IActivate)_subject).Activate();
+            _subject.ActivateItem(new ClippingViewModel(_mockSessionManager.Object) { Model = clippingPresenter1});
+            _subject.ActivateItem(new ClippingViewModel(_mockSessionManager.Object) { Model = clippingPresenter2});
+
+            _subject.FilteredItems.Count.Should().Be(2);
+        }
+
+        [Test]
+        public void FilteredItems_WhenFilterTextHasValue_FiltersItemsByContentContainingText()
+        {
+            var clippingPresenter1 = new ClippingPresenter(new ClippingModel { Content = "test1" });
+            var clippingPresenter2 = new ClippingPresenter(new ClippingModel { Content = "Other" });
+            ((IActivate)_subject).Activate();
+            _subject.ActivateItem(new ClippingViewModel(_mockSessionManager.Object) { Model = clippingPresenter1});
+            _subject.ActivateItem(new ClippingViewModel(_mockSessionManager.Object) { Model = clippingPresenter2});
+
+            _subject.FilterText = "oth";
+
+            _subject.FilteredItems.Count.Should().Be(1);
+            _subject.FilteredItems.Cast<IClippingViewModel>().First().Model.Should().Be(clippingPresenter2);
+        }
+
+        [Test]
+        public void FilteredItems_WhenAllFilterOptionsAreFalse_ReturnsAllItems()
+        {
+            AddClippingsWithFilterCombinations();
+
+            _subject.ShowCloudClippings = false;
+            _subject.ShowLocalClippings = false;
+            _subject.ShowStarred = false;
+
+            _subject.FilteredItems.Count.Should().Be(4);
+        }
+
+        [Test]
+        public void FilteredItems_WhenShowCloudClippingsIsSelected_ReturnsOnlyCloudClippings()
+        {
+            AddClippingsWithFilterCombinations();
+
+            _subject.ShowCloudClippings = true;
+            _subject.ShowLocalClippings = false;
+            _subject.ShowStarred = false;
+
+            _subject.FilteredItems.Count.Should().Be(2);
+            _subject.FilteredItems.Cast<IClippingViewModel>()
+                .All(vm => vm.Model.BackingModel.Source == Clipping.ClippingSourceEnum.Cloud)
+                .Should()
+                .BeTrue();
+        }
+
+        [Test]
+        public void FilteredItems_WhenShowLocalClippingsIsSelected_ReturnsOnlyLocalClippings()
+        {
+            AddClippingsWithFilterCombinations();
+
+            _subject.ShowCloudClippings = false;
+            _subject.ShowLocalClippings = true;
+            _subject.ShowStarred = false;
+
+            _subject.FilteredItems.Count.Should().Be(2);
+            _subject.FilteredItems.Cast<IClippingViewModel>()
+                .All(vm => vm.Model.BackingModel.Source == Clipping.ClippingSourceEnum.Local)
+                .Should()
+                .BeTrue();
+        }
+
+        [Test]
+        public void FilteredItems_WhenShowCloudClippingsAndShowStarredAreSelected_ReturnsOnlyStarredCloudClippings()
+        {
+            AddClippingsWithFilterCombinations();
+
+            _subject.ShowCloudClippings = true;
+            _subject.ShowLocalClippings = false;
+            _subject.ShowStarred = true;
+
+            _subject.FilteredItems.Count.Should().Be(1);
+            _subject.FilteredItems.Cast<IClippingViewModel>()
+                .All(vm => vm.Model.BackingModel.Source == Clipping.ClippingSourceEnum.Cloud && vm.Model.IsStarred)
+                .Should()
+                .BeTrue();
+        }
+
+        private void AddClippingsWithFilterCombinations()
+        {
+            _subject.ActivateItem(new ClippingViewModel(_mockSessionManager.Object) { Model = new ClippingPresenter(new ClippingModel { Content = "1", IsStarred = false, Source = Clipping.ClippingSourceEnum.Cloud })});
+            _subject.ActivateItem(new ClippingViewModel(_mockSessionManager.Object) { Model = new ClippingPresenter(new ClippingModel { Content = "2", IsStarred = true, Source = Clipping.ClippingSourceEnum.Local })});
+            _subject.ActivateItem(new ClippingViewModel(_mockSessionManager.Object) { Model = new ClippingPresenter(new ClippingModel { Content = "3", IsStarred = true, Source = Clipping.ClippingSourceEnum.Cloud })});
+            _subject.ActivateItem(new ClippingViewModel(_mockSessionManager.Object) { Model = new ClippingPresenter(new ClippingModel { Content = "4", IsStarred = false, Source = Clipping.ClippingSourceEnum.Local })});
         }
     }
 }
