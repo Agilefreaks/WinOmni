ecf74c88340a6069b4780cc12d28f8638c086b0a
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2015-02-20 08:59:05 +0200

    removes not used resource key

--- Code/Omnipaste/Properties/Resources.Designer.cs
+++ Code/Omnipaste/Properties/Resources.Designer.cs
@@ -992,15 +992,6 @@ namespace Omnipaste.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to .
-        /// </summary>
-        public static string String1 {
-            get {
-                return ResourceManager.GetString("String1", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to To connect your devices, you need an authorization code. .
         /// </summary>
         public static string UserTokenLabel1 {
