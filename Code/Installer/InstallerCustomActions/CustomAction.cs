1665977aba28e47592a751d030132742446f24ef
adi <levelboy@gmail.com>
2015-02-24 11:21:34 +0200

    Removed ClickOnce related code
    Removed unused code

--- Code/Installer/InstallerCustomActions/CustomAction.cs
+++ Code/Installer/InstallerCustomActions/CustomAction.cs
@@ -1,50 +1,10 @@
 ﻿namespace InstallerCustomActions
 {
     using System;
-    using System.IO;
-    using InstallerCustomActions.ClickOnceMigration;
-    using InstallerCustomActions.ClickOnceUninstaller;
     using Microsoft.Deployment.WindowsInstaller;
 
     public class CustomActions
     {
-        [CustomAction]
-        public static ActionResult UninstallClickOnce(Session session)
-        {
-            ActionResult result;
-            try
-            {
-                var productName = session.CustomActionData["ProductName"];
-                var uninstallInfo = UninstallInfo.Find(productName);
-                if (uninstallInfo != null)
-                {
-                    var migrationResult = CustomizedClickOnceUninstaller.Uninstall(productName, session.CustomActionData["PublisherName"]);
-                    if (migrationResult == MigrationStepResultEnum.Success)
-                    {
-                        result = ActionResult.Success;
-                    }
-                    else
-                    {
-                        session.Log(CustomizedClickOnceUninstaller.LastException ?? string.Empty);
-                        throw new Exception("Could not migrate existing click once application. Failed with: " + migrationResult);
-                    }
-                                 
-                }
-                else
-                {
-                    session.Log("No existing ClickOnce installation found");
-                    result = ActionResult.NotExecuted;
-                }
-            }
-            catch (Exception exception)
-            {
-                session.Log(exception.ToString());
-                result = ActionResult.Failure;
-            }
-
-            return result;
-        }
-
         [CustomAction]
         public static ActionResult SetStartArguments(Session session)
         {
