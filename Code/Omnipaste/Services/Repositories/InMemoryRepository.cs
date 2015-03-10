0d706197d621c8f70ea579246f2556a9c3768baf
Calin <calinoiu.alexandru@agilefreaks.com>
2015-03-10 15:05:05 +0200

    [wo-427] updates InMemoryRepository

--- Code/Omnipaste/Services/Repositories/InMemoryRepository.cs
+++ Code/Omnipaste/Services/Repositories/InMemoryRepository.cs
@@ -1,6 +1,7 @@
 namespace Omnipaste.Services.Repositories
 {
     using System;
+    using System.Collections.Concurrent;
     using System.Collections.Generic;
     using System.Linq;
     using System.Reactive.Linq;
@@ -9,106 +10,81 @@ namespace Omnipaste.Services.Repositories
     using OmniCommon.Helpers;
     using Omnipaste.Models;
 
-    public abstract class InMemoryRepository<T> : IRepository<T>
+    public abstract class InMemoryRepository<T> : BaseRepository<T>
         where T : BaseModel
     {
-        private readonly IDictionary<string, T> _items;
-
-        private readonly Subject<RepositoryOperation<T>> _subject;
+        private readonly ConcurrentDictionary<string, T> _items;
 
         protected InMemoryRepository()
         {
-            _subject = new Subject<RepositoryOperation<T>>();
-            _items = new Dictionary<string, T>();
+            _items = new ConcurrentDictionary<string, T>();
         }
 
-        public IObservable<RepositoryOperation<T>> OperationObservable
-        {
-            get
-            {
-                return _subject;
-            }
-        }
-
-        public IObservable<RepositoryOperation<T>> Save(T item)
+        public override IObservable<RepositoryOperation<T>> Save(T item)
         {
             return Async(() => SaveSynchronous(item));
         }
 
-        public IObservable<T> Get(string id)
+        public override IObservable<T> Get(string id)
         {
             return Async(() => GetSynchronous(id));
         }
 
-        public IObservable<T> Get(Func<T, bool> match)
+        public override IObservable<T> Get(Func<T, bool> match)
         {
             return Async(() => GetSynchronous(match));
         }
 
-        public IObservable<IEnumerable<T>> GetAll()
+        public override IObservable<IEnumerable<T>> GetAll()
         {
             return GetAll(_ => true);
         }
 
-        public IObservable<IEnumerable<T>> GetAll(Func<T, bool> filter)
+        public override IObservable<IEnumerable<T>> GetAll(Func<T, bool> filter)
         {
             return Async(() => GetAllSynchronous(filter));
         }
 
-        public IObservable<RepositoryOperation<T>> Delete(string id)
+        public override IObservable<RepositoryOperation<T>> Delete(string id)
         {
             return Async(() => DeleteSynchronous(id));
         }
 
         public List<T> GetAllSynchronous(Func<T, bool> filter)
         {
-            lock (_items)
-            {
-                return _items.Values.Where(filter).ToList();
-            }
+            return _items.Values.Where(filter).ToList();
         }
 
         public T GetSynchronous(string id)
         {
-            lock (_items)
-            {
-                return _items.GetValueOrDefault(id);
-            }
+            return _items.GetValueOrDefault(id);
         }
 
         public T GetSynchronous(Func<T, bool> match)
         {
-            lock (_items)
-            {
-                return _items.Values.First(match);
-            }
+            return _items.Values.First(match);
         }
 
         public RepositoryOperation<T> SaveSynchronous(T item)
         {
-            lock (_items)
-            {
-                var value = _items.GetValueOrDefault(item.UniqueId);
-                var operation = value == null ? RepositoryMethodEnum.Create : RepositoryMethodEnum.Update;
-                var result = new RepositoryOperation<T>(operation, item);
-                _items[item.UniqueId] = item;
-                _subject.OnNext(result);
+            var value = _items.GetValueOrDefault(item.UniqueId);
+            var operation = value == null ? RepositoryMethodEnum.Create : RepositoryMethodEnum.Update;
+            var result = new RepositoryOperation<T>(operation, item);
+            _items.AddOrUpdate(item.UniqueId, item, (key, existingValue) => item);
+            _subject.OnNext(result);
 
-                return result;
-            }
+            return result;
         }
 
         public RepositoryOperation<T> DeleteSynchronous(string id)
         {
-            lock (_items)
-            {
-                var targetItem = _items.GetValueOrDefault(id);
-                _items.Remove(id);
-                var result = new RepositoryOperation<T>(RepositoryMethodEnum.Delete, targetItem);
-                _subject.OnNext(result);
+            var targetItem = _items.GetValueOrDefault(id);
+            T removedValue;
+            _items.TryRemove(id, out removedValue);
+            var result = new RepositoryOperation<T>(RepositoryMethodEnum.Delete, targetItem);
+            _subject.OnNext(result);
 
-                return result;
-            }
+            return result;
         }
 
         private IObservable<TResult> Async<TResult>(Func<TResult> funcToExecute)
---
+++ Code/Omnipaste/Services/Repositories/SercureRepository.cs
@@ -0,0 +1,48 @@
+﻿namespace Omnipaste.Services.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using Omnipaste.Models;
+
+    public class SercureRepository<T> : IRepository<T>
+        where T : BaseModel
+    {
+        public IObservable<RepositoryOperation<T>> OperationObservable
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IObservable<RepositoryOperation<T>> Save(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObservable<RepositoryOperation<T>> Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObservable<T> Get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObservable<T> Get(Func<T, bool> match)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObservable<IEnumerable<T>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObservable<IEnumerable<T>> GetAll(Func<T, bool> filter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
