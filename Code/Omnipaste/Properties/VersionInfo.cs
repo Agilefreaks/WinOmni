777a74563e89483fa2bdad4ff95ea56df30c7351
Calin <calinoiu.alexandru@agilefreaks.com>
2015-03-25 22:05:10 +0200

    increases version number

--- Code/Omnipaste/Properties/VersionInfo.cs
+++ Code/Omnipaste/Properties/VersionInfo.cs
@@ -1,2 +1,2 @@
-﻿[assembly: System.Reflection.AssemblyVersion("1.4.1.0")]
-[assembly: System.Reflection.AssemblyFileVersion("1.4.1.0")]
+﻿[assembly: System.Reflection.AssemblyVersion("1.4.2.0")]
+[assembly: System.Reflection.AssemblyFileVersion("1.4.2.0")]
