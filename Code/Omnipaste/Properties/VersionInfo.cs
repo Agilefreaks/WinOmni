40279837f0852061ebde26a58d90a6c8029e0f60
Calin <calinoiu.alexandru@agilefreaks.com>
2015-03-31 12:20:42 +0300

    updates version number

--- Code/Omnipaste/Properties/VersionInfo.cs
+++ Code/Omnipaste/Properties/VersionInfo.cs
@@ -1,2 +1,2 @@
-﻿[assembly: System.Reflection.AssemblyVersion("1.5.1.0")]
-[assembly: System.Reflection.AssemblyFileVersion("1.5.1.0")]
+﻿[assembly: System.Reflection.AssemblyVersion("1.5.2.0")]
+[assembly: System.Reflection.AssemblyFileVersion("1.5.2.0")]
